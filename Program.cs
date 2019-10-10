25fa144c32c7859e559b19178c3f42cd34ae1b4a
Maggie Johnson <mmjohn09@icloud.com>
2019-10-10 18:43:08 -0500

    finished aggregate operations

--- Program.cs
+++ Program.cs
@@ -73,6 +73,42 @@ namespace linq_list
                 Console.WriteLine(num);
             }
 
+            // Output how many numbers are in this list
+            List<int> someMoreNumbers = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+
+            int numberCount = someMoreNumbers.Count();
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Count of numbers:");
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine(numberCount);
+
+            // How much money have we made?
+            List<double> purchases = new List<double>()
+            {
+                2340.29, 745.31, 21.76, 34.03, 4786.45, 879.45, 9442.85, 2454.63, 45.65
+            };
+
+            double totalPurchasesAmount = purchases.Sum();
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Total Purchases:");
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine($"${totalPurchasesAmount}");
+
+            // What is our most expensive product?
+            List<double> prices = new List<double>()
+            {
+                879.45, 9442.85, 2454.63, 45.65, 2340.29, 34.03, 4786.45, 745.31, 21.76
+            };
+
+            double highestPrice = prices.Max();
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Highest Price:");
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine($"${highestPrice}");
+
         }
     }
 }
