391db568f44c30a7778491d7d7d8ed96f3d11420
Maggie Johnson <mmjohn09@icloud.com>
2019-10-10 19:25:42 -0500

    added list of millionaires

--- Program.cs
+++ Program.cs
@@ -120,17 +120,43 @@ namespace linq_list
                 66, 12, 8, 27, 82, 34, 7, 50, 19, 46, 81, 23, 30, 4, 68, 14
             };
 
-            List<int> numsBeforeSquare = wheresSquaredo.TakeWhile(num => Math.Sqrt(num) % 1 !=0).ToList();
-            
+            List<int> numsBeforeSquare = wheresSquaredo.TakeWhile(num => Math.Sqrt(num) % 1 != 0).ToList();
+
             Console.WriteLine("-----------------------------------");
             Console.WriteLine("Numbers til square:");
             Console.WriteLine("-----------------------------------");
 
             foreach (int num in numsBeforeSquare)
             {
-            Console.WriteLine(num);
+                Console.WriteLine(num);
             }
-                
+
+
+            // Build a collection of customers who are millionaires
+
+            List<Customer> customers = new List<Customer>()
+            {
+            new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
+            new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
+            new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
+            new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
+            new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
+            new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
+            new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
+            new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
+            new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
+            new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
+            };
+
+            List<Customer> millionaires = customers.Where(customer => customer.Balance >= 1000000).ToList();
+
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("List of Millionaires:");
+            Console.WriteLine("-----------------------------------");
+            foreach (Customer customer in millionaires)
+            {
+                Console.WriteLine(customer.Name);
+            }
 
         }
     }
